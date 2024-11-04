0c30af9d3f26a27fc05ddf9a1af0aa1347c2dafd
ihtiandr9 (note) <ihtiandr9@yandex.ru>
2024-11-04 20:53:54 +0800

    bkasm/feature: unittests

--- bkasm/src/tests.py
+++ bkasm/src/tests.py
@@ -1,6 +1,12 @@
  ## Test script on python
 
+def printwarning(warning):
+    print("Warning:", warning)
+
 def main():
-    print("Running tests")
+    for i in range(10):
+        print("test", i, sep="")
+
+    printwarning("testof warnning")
 
 main()
